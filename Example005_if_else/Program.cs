5d9ecdf2fefb274ad1e31bb295df36b3312276d9
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-01-26 16:58:35 +0300

    Folder the 'Example005_if_else' had been added

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hooray! It's Masha!");
+}
+else
+{
+    Console.WriteLine("Hello, ");
+    Console.WriteLine(username);
+}
